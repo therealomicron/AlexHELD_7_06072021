9c180ced837f148699f72b69ec3b077bebc34931
Alex <heldalex@outlook.com>
2021-07-12 15:53:20 +0200

    mysql database functional

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
-require('dotenv').config();
+require('dotenv').config(); //masks sensitive information
+const cors = require('cors');
+const corsOptions = {
+	origin: "http://localhost:8081"
+};
 const express = require('express');
-const path = require('path');
+const app = express();
+const path = require('path'); //module for working with file and directory paths
 
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('p_sept', process.env.DB_USER, process.env.DB_PASS, {
@@ -8,18 +13,22 @@ const sequelize = new Sequelize('p_sept', process.env.DB_USER, process.env.DB_PA
 	dialect: 'mysql'
 });
 
-const indexRouter = require('./routes/submissions');
-const usersRouter = require('./routes/users');
 
-const app = express();
+//const submissionsRouter = require('./routes/submissions');
+//const usersRouter = require('./routes/users');
+//app.use('/', submissionRouter);
+//app.use('/users', usersRouter);
+const db = require("./models/index");
+db.sequelize.sync();
 
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
 
 module.exports = app;
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,27 +1,6 @@
-const dbConfig = require("../config/db.config.js");
-
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-	host: dbConfig.HOST,
-	dialect: dbConfig.dialect,
-	operatorsAliases: false,
-
-	pool: {
-		max: dbConfig.pool.max,
-		min: dbConfig.pool.min,
-		acquire: dbConfig.pool.acquire,
-		idle: dbConfig.pool.idle
-	}
-});
-
-const db = {};
-
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-
-User.init({
-	//attributes
-	pseudo: {
+module.exports = (sequelize, Sequelize) => {
+	const User = sequelize.define("user", {
+			pseudo: {
 		type: Sequelize.STRING,
 		allowNull: false,
 		unique: true
@@ -30,8 +9,8 @@ User.init({
 		type: Sequelize.STRING,
 		allowNull: false,
 		unique: false
-	},
-}, {
-	sequelize,
-	modelName: 'user'
+	}
 });
+
+return User;
+};
