5f71a9968423b089972f23ce1d2899d2278a820e
Alex <heldalex@outlook.com>
2021-07-20 10:27:58 +0200

    404 error corrected, new 500err ER_BAD_FIELD_ERROR

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const corsOptions = {
 };
 const morgan = require('morgan');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const path = require('path'); //module for working with file and directory paths
 
@@ -14,20 +15,21 @@ const sequelize = new Sequelize('p_sept', process.env.DB_USER, process.env.DB_PA
 	dialect: 'mysql'
 });
 
-
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+//app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 //const submissionsRouter = require('./routes/submissions');
 const usersRouter = require('./routes/users');
 //app.use('/', submissionRouter);
-app.use('/users', usersRouter);
+app.use('/api/auth/users', usersRouter);
 const db = require("./models/index");
 db.sequelize.sync();
 
 
 
-app.use(express.json());
-app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,44 +4,43 @@ const bcrypt = require('bcrypt');
 const user = require('../models/user');
 const User = db.users;
 const Op = db.Sequelize.Op;
-//const User = require('../models/user');
-//const jwt = require('jsonwebtoken');
-//const bcrypt = require('bcrypt');
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10).then(
-      (hash) => {
-        const user = new User({
-          pseudo: req.body.pseudo,
-          hashedPassword: hash,
-          isAdmin: 0
-        });
-        user.save().then(
-          () => {
-            res.status(201).json({
-              message: 'User added successfully!'
-            });
-          }
-        ).catch(
-          (error) => {
-            res.status(500).json({
-              error: error
-            });
-          }
-        );
-      }
-    );
+  console.log("Signup controller called");
+  console.log(req.query.hashedPassword);
+  bcrypt.hash(req.query.hashedPassword, 10).then(
+    (hash) => {
+      const user = new User({
+        pseudo: req.query.pseudo,
+        hashedPassword: hash,
+        isAdmin: 0
+      });
+      user.save().then(
+        () => {
+          res.status(201).json({
+            message: 'User added successfully!'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(500).json({
+            error: error
+          });
+        }
+      );
+    }
+  );
 };
 
 exports.login = (req, res, next) => {
-    User.findOne({ pseudo: req.body.pseudo }).then(
+    User.findOne({ pseudo: req.query.pseudo }).then(
       (user) => {
         if (!user) {
           return res.status(401).json({
             error: new Error('User not found!')
           });
         }
-        bcrypt.compare(req.body.password, user.hashedPassword).then(
+        bcrypt.compare(req.query.hashedPassword, user.hashedPassword).then(
           (valid) => {
             if (!valid) {
               return res.status(401).json({
