e08d47a4b5aad682c14278da12919eebb00bdd47
Alex <heldalex@outlook.com>
2021-07-21 15:41:29 +0200

    auth added to user delete route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+const auth = require('../middleware/auth');
 
 const userCtrl = require('../controllers/users');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
-router.delete('/suppression', userCtrl.suppression);
+router.delete('/suppression', auth, userCtrl.suppression);
 
 module.exports = router;
