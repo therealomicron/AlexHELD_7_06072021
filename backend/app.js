0fca7c4ef4d6f0253f9f958ab9bf90c24f2824fa
Alex <heldalex@outlook.com>
2021-08-01 20:47:48 +0200

    rate limit increased

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
-	max: 100
+	max: 1000
   });
 app.use(helmet()); //sets HTTP headers
 app.use(helmet.contentSecurityPolicy({
