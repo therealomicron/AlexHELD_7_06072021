c598ce3c1f43a241652d7a331d022555f08ac9e8
Alex <heldalex@outlook.com>
2021-08-01 13:43:14 +0200

    login page fully functional

--- backend/frontend/assets/js/login.js
+++ backend/frontend/assets/js/login.js
@@ -26,28 +26,27 @@ const connecter = function (url) {
     })
 }
 
-const sinscrire = function (url) {
+async function sinscrire(url) {
     const uid = document.querySelector("#pseudo");
     const mdp = document.querySelector("#hashedPassword");
-    return new Promise(function (resolve, reject) {
-        let req = new XMLHttpRequest()
-        req.open('POST', url, true)
-        req.onreadystatechange = function () {
-            if (req.readyState == 4) {
-                if (req.status == 200) {
-                    resolve(JSON.parse(req.response))
-                } else {
-                    reject(req)
-                }
-            }
-        };
-        req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-        req.send(JSON.stringify({
-            "pseudo": uid.value,
-            "hashedPassword": mdp.value
-        }));
-    })
+    const response = await fetch(url, {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({
+            pseudo: uid.value,
+            hashedPassword: mdp.value
+        })
+    });
+    return response;
 }
+
 window.onload = () => {
     console.log("onLoad function called");
     const loginButton = document.querySelector("#connecter");
@@ -63,7 +62,9 @@ window.onload = () => {
             }
         )
     );
-    signupButton.addEventListener("click", () => connecter(signupUrl).then(
+    signupButton.addEventListener("click", () => sinscrire(signupUrl).then(
+            response => {
+                if (response.status == 201) {
             connecter(loginUrl).then(
                 value => {
                 console.log(value);
@@ -73,7 +74,9 @@ window.onload = () => {
                 error => {
                     console.log(error);
                 }
-            )
+            )} else {
+                console.log("echec");
+            }}
         ).catch(
             error => {
                 console.log(error);
